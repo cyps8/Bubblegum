ca664c13db61fac2853fdf89db458feb459f1576
Cyp P <cypriane81@gmail.com>
2025-01-25 17:35:02 +0000

    [+Added] kinda like actually game 🫧🫧🫧

---
+++ Scripts/SceneMan.gd
@@ -0,0 +1,23 @@
+class_name SceneMan extends Node
+
+@export var main: PackedScene
+
+@export var game: PackedScene
+
+var currentScene: Node2D
+
+static var ins: SceneMan
+
+func _init():
+	ins = self
+
+func _ready():
+	ChangeScene(main)
+
+func ChangeScene(scene: PackedScene):
+	if currentScene != null:
+		remove_child(currentScene)
+		currentScene.queue_free()
+	var newScene = scene.instantiate() as Node2D
+	add_child(newScene)
+	currentScene = newScene
